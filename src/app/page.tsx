82f8b97c15f37c38cbaa8b196eef9aa73f55c531
mohamedkhameSss <khamessm24@gmail.com.com>
2024-06-24 05:53:27 +0200

    heroSec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Icons from "@/components/Icons";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Phone from "@/components/Phone";
 import { CheckIcon, StarFilledIcon, StarIcon } from "@radix-ui/react-icons";
@@ -14,7 +15,7 @@ export default function Home() {
                 relative mx-auto text-center lg:text-left
                 flex flex-col items-center lg:items-start
                 ">
-                  <div className="absolute w-28 left-0 top-20 hidden lg:block">
+                  <div className="absolute w-28 -left-0 -top-20 hidden lg:block">
                     <img src="/public/snake-1.png" className="w-full" />
                   </div>
                   <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">Your Image on a <span className="bg-green-600 px-2 text-white">Custom</span> Phane Case</h1>
@@ -64,16 +65,28 @@ export default function Home() {
                     <p><span className="font-semibold">1.250</span> happy customer</p>
                   </div>
                 </div>
-                <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mt-20 h-fit">
-                <div className="relative md:max-w-xl">
+              </div>
+                <div className="col-span-full lg:col-span-1 w-full flex 
+                justify-center px-8 sm:px-16 lg:mx-0 md:px-0 mt-32 lg:mt-20 h-fit">
+                <div className="relative md:max-w-xl ">
                   <img src="/public/your-image.png" className="absolute w-40
                   lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block
                   " alt="" />
-                  <img src="/public/line.png" className="absolute w-20 left-6 -bottom-6 select-none" alt="" />
+                  <img src="/public/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" alt="" />
                   <Phone className="w-64" imgSrc="/public/testimonials/1.jpg"/>
                 </div>
                 </div>
-              </div>
+              </MaxWidthWrapper>
+            </section>
+            {/* value proposition section */}
+            <section className="bg-slate-100 py-24">
+              <MaxWidthWrapper className="flex flex-col items-center gap-16 sm:gap-32">
+                <div className="flex flex-col lg:flex-row items-center gap-4 sm:gap-6">
+                      <h2 className="order-1 mt-2 tracking-tight text-center text-balance
+                      !leading-tight font-bold text-5xl md:text-6xl text-gray-900">What our <span className="relative px-2"> customer <Icons.underline className='
+                      hidden sm:block pointer-events-none absolute inset-x-0 -bottom-6 text-green-500
+                      '/></span> say</h2>
+                </div>
               </MaxWidthWrapper>
             </section>
            </div>
