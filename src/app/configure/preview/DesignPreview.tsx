91fcb38a353fbc8966c710448ab32f5c886574d9
mohamedkhameSss <113071196+mohamedkhameSss@users.noreply.github.com>
2024-07-21 00:07:12 +0300

    user kinde edits

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -59,15 +59,14 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   });
 
   const handleCheckout = () => {
-    createPaymentSession({ configId: id });
-    // if (user) {
-    //   // create payment session
-    //   createPaymentSession({ configId: id });
-    // } else {
-    //   // need to log in
-    //   localStorage.setItem("configurationId", id);
-    //   setIsLoginModalOpen(true);
-    // }
+    if (user) {
+      // create payment session
+      createPaymentSession({ configId: id });
+    } else {
+      // need to log in
+      localStorage.setItem("configurationId", id);
+      setIsLoginModalOpen(true);
+    }
   };
 
   return (
