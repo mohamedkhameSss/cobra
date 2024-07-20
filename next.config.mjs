4b60abb7ba5b7a60b171a6e7bc4cf438eb209016
mohamedkhameSss <113071196+mohamedkhameSss@users.noreply.github.com>
2024-07-21 01:46:03 +0300

    access-control

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
   },
   headers: [
     { key: "Access-Control-Allow-Credentials", value: "true" },
-    { key: "Access-Control-Allow-Origin", value: "*" },
+    { key: "Access-Control-Allow-Origin", value: "https://cobra-gamma.vercel.app" },
     {
       key: "Access-Control-Allow-Methods",
       value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
