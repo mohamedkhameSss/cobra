da0cdbd85be552337e43516e08e9cb5e64f91a6c
mohamedkhameSss <113071196+mohamedkhameSss@users.noreply.github.com>
2024-07-21 19:23:43 +0300

    update index db

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,12 +3,12 @@ declare global {
     var cachedPrisma:PrismaClient
 }
 let prisma :PrismaClient
-if(process.env.NODE_ENV !== 'development'){
-    prisma=new PrismaClient()
-}else{
+// if(process.env.NODE_ENV !== 'development'){
+//     prisma=new PrismaClient()
+// }else{
     if(!global.cachedPrisma){
         global.cachedPrisma=new PrismaClient()
     }
     prisma = global.cachedPrisma
-}
+// }
 export const db =prisma
